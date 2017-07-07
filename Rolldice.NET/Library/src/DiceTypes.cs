04ecfd1dcae3ff7cd7bdc35d28bb1019a35ad3da
Matthew Brown <lokislayer@gmail.com>
2017-07-07 12:15:17 -0500

    Enum Type
    Which dice I will be allowed to be used in the application. I also created a extnesion function that will tell if a certain type is allowed or not.

---
+++ Rolldice.NET/Library/src/DiceTypes.cs
@@ -0,0 +1,47 @@
+﻿namespace Library
+{
+    /// <summary>
+    /// Which dice that are allowed to be used
+    /// For example, D3,D4,etc
+    /// </summary>
+    public enum DiceTypes
+    {
+        None,
+        D3 = 3,
+        D4 = 4,
+        D6 = 6,
+        D10 = 10,
+        D12 = 12,
+        D20 = 20,
+        D100 = 100
+    }
+
+    /// <summary>
+    /// Static Class to house my extension method for me to test weather a certain type is allowed or not
+    /// </summary>
+    public static class Extensions
+    {
+        public static bool Allowed(this DiceTypes type)
+        {
+            bool retValue = false;
+            switch (type)
+            {
+                case DiceTypes.None:
+                    break;
+                case DiceTypes.D3:
+                case DiceTypes.D4:
+                case DiceTypes.D6:
+                case DiceTypes.D10:
+                case DiceTypes.D12:
+                case DiceTypes.D20:
+                case DiceTypes.D100:
+                {
+                    retValue = true;
+                    break;
+                }
+            }
+
+            return retValue;
+        }
+    }
+}
