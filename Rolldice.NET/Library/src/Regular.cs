eb9c2bd7607ecbd86e0041e96b24644f03d24163
Matthew Brown <lokislayer@gmail.com>
2017-07-10 04:33:11 -0500

    A simple Die impentation
    This die implementation is for regular dice being rolled without the worry if they explode,etc.

---
+++ Rolldice.NET/Library/src/Regular.cs
@@ -0,0 +1,16 @@
+﻿namespace Library
+{
+    /// <summary>
+    /// This is what a typical die would represent.
+    /// Therefore this would be used if the results
+    /// rolled do not have to worry about it exploding
+    /// or not.
+    /// </summary>
+    public class Regular : Dice
+    {
+        public override int RollDie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
