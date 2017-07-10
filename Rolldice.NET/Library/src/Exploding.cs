b8ae1748dedf529ac2570b1b2d5ca4be644e1dca
Matthew Brown <lokislayer@gmail.com>
2017-07-10 04:32:10 -0500

    Exploding Die implementation
    This is the implemention of what an exploding die would be like using my library.

---
+++ Rolldice.NET/Library/src/Exploding.cs
@@ -0,0 +1,17 @@
+﻿namespace Library
+{
+    /// <summary>
+    /// If a die has the ability to explode will explode
+    /// only when the value rolled equals the max value 
+    /// the dice can be rolled. It then rolls again until
+    /// the next result does not equal the max value of 
+    /// the die.
+    /// </summary>
+    public class Exploding : Dice
+    {
+        public override int RollDie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
