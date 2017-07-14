447b2c533bb5bcb726f8541cd1fa4e6913a7b7c3
Matthew Brown <lokislayer@gmail.com>
2017-07-14 12:40:22 -0500

    Implemented
    Regular is defined and actually simple class since all it does is roll the dice already defined. If not it will throw an exception.

--- Rolldice.NET/Library/src/Regular.cs
+++ Rolldice.NET/Library/src/Regular.cs
@@ -1,4 +1,6 @@
-﻿namespace Library
+﻿using System;
+
+namespace Library
 {
     /// <summary>
     /// This is what a typical die would represent.
@@ -8,9 +10,26 @@
     /// </summary>
     public class Regular : Dice
     {
+        /// <summary>
+        /// Constructor for this object
+        /// </summary>
+        /// <param name="die">Enum type describing which type of Die this represents</param>
+        public Regular(DiceTypes die)
+        {
+            Die = die;
+            Sides = Die.ConvertToInt();
+        }
+
+        /// <summary>
+        /// Implementation of the abstract RollDie method which in turns call the concrete
+        /// RollDie method defined in the base class.
+        /// </summary>
+        /// <returns>Integer of the Rolled Dice in question .. [1..N]</returns>
         public override int RollDie()
         {
-            throw new System.NotImplementedException();
+            if (Die == DiceTypes.None)
+                throw new ArgumentException("It has been initialized properly yet.");
+            return RollDie(Die);
         }
     }
 }
