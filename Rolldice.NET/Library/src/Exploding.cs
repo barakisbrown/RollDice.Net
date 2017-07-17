d1f4b67180557b4603db5b204d0088a98b3d7cf5
Matthew Brown <lokislayer@gmail.com>
2017-07-17 12:01:42 -0500

    Completed constructor but still not done.
    I have completed work on the constructor but the heart of the class is the RollDie(). I have not decided if its going to be a loop or recursion based.

--- Rolldice.NET/Library/src/Exploding.cs
+++ Rolldice.NET/Library/src/Exploding.cs
@@ -9,6 +9,21 @@
     /// </summary>
     public class Exploding : Dice
     {
+        private int _explodTargetNumber;
+
+        /// <summary>
+        /// Constructor needing what dice is rolled and what is the target number that is consider to explode
+        /// or rolled again
+        /// </summary>
+        /// <param name="diceToBeRolled">Type of Dice to be Rolled</param>
+        /// <param name="explodeTargetNumber">Value which a dice must equal or beat to explode(roll again)</param>
+        public Exploding(DiceTypes diceToBeRolled, int explodeTargetNumber)
+        {
+            Die = diceToBeRolled;
+            Sides = Die.ConvertToInt();
+            _explodTargetNumber = explodeTargetNumber;
+        }
+
         public override int RollDie()
         {
             throw new System.NotImplementedException();
