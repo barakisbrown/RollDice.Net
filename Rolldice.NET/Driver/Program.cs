a0bbdb04221225d8eef19af0e9ffae152f8ce72b
Matthew Brown <lokislayer@gmail.com>
2017-07-19 07:30:57 -0500

    Implementation to show that it is working

---
+++ Rolldice.NET/Driver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Dice = new Regular(DiceTypes.D6);
+            for (int i = 0; i < 10; i++)
+            {                
+                Console.WriteLine("Result = {0}",Dice.RollDie());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
