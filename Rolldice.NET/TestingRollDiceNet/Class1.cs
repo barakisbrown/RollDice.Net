36b8407940c08741471118f8e700d07e458cd78b
Matthew Brown <lokislayer@gmail.com>
2017-07-19 07:31:24 -0500

    Adding Testing for my Library

---
+++ Rolldice.NET/TestingRollDiceNet/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestingRollDiceNet
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2,2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2,2));
+        }
+
+        [Theory]
+        [InlineData((3))]
+        [InlineData((5))]
+        [InlineData((6))]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        private int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        private bool IsOdd(int value) => value % 2 == 1;
+    }
+}
---
+++ Rolldice.NET/TestingRollDiceNet/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using Xunit;
+
+namespace TestingRollDiceNet
+{
+    public class TestClass
+    {
+        [Theory]
+        [InlineData(DiceTypes.D6)]
+        [InlineData(DiceTypes.D10)]
+        public void TestRegularDice(DiceTypes value)
+        {
+            var Dice = new Regular(value);
+
+            Assert.InRange(Dice.RollDie(),1,value.ConvertToInt());
+        }
+
+        [Fact]
+        public void TestWrongDiceType()
+        {
+            var TestDice = Extensions.ConvertToTypes(5);
+            Assert.Equal(DiceTypes.None,TestDice);
+        }
+    }
+}
