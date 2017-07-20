733eed1c300e6b55d5fcd5d54f8b2947dc943758
Matthew Brown <lokislayer@gmail.com>
2017-07-19 20:49:41 -0500

    Success Based Dice Rolling

---
+++ Rolldice.NET/Library/src/Success.cs
@@ -0,0 +1,30 @@
+﻿namespace Library
+{
+    /// <summary>
+    /// Success Dice are for any games that use Success to determine an outcome.
+    /// Ex. White Wolf / Shadowrun / Savage Worlds
+    /// </summary>
+    public class Success : Dice
+    {
+        private readonly int _targetNumber;
+
+        public Success(DiceTypes type, int targetNumber)
+        {
+            Die = type;
+            _targetNumber = targetNumber;
+            Initialize();
+        }
+
+        /// <summary>
+        /// Take the Die and rolls it. Compares it to a TN.
+        /// If it equal or greater than, we have a winner
+        /// </summary>
+        /// <returns>1 for Success / 0 for Failure</returns>
+        public override int RollDie()
+        {
+            var diceRolled = RollDie(Die);
+
+            return (diceRolled >= _targetNumber) ? 1 : 0;
+        }
+    }
+}
